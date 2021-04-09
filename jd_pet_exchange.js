9f4a8fa770abdaf3e2b00f71fe1b58c1c1bb2392
kyo47957 <34297798+kyo47957@users.noreply.github.com>
2021-04-09 18:40:39 +0800

    Update jd_pet_exchange.js

--- jd_pet_exchange.js
+++ jd_pet_exchange.js
@@ -190,9 +190,8 @@ function getJDServerTime() {
 
 async function get_diff_time() {
   console.log(`本机时间戳 ${Date.now()}`);
-  var d1=new Date(`2021-04-09 18:10:00`);
-  var d=(new Date('2021-04-09 18:10:00')).getTime();
-  console.log(`时间戳 ${d1}`);
+  var d=(new Date(2021,4,9,18,45,0)).getTime();
+  console.log(`时间戳 ${d}`);
   var jd=await getJDServerTime();
   console.log(`京东服务器时间戳 ${jd}`);
   return d - jd;
